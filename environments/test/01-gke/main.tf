e160f22da27537c5e63f27b374144bd9ac0fe0ba
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2025-04-08 16:16:50 +0100

    test environment: more space test environment: larger network

--- environments/test/01-gke/main.tf
+++ environments/test/01-gke/main.tf
@@ -1,36 +1,40 @@
 // https://cloud.google.com/kubernetes-engine/docs/concepts/alias-ips#cluster_sizing_secondary_range_pods
-// maximum cluster size: 64 autopilot pods
 module "autopilot" {
   source      = "../../../modules/k8s/"
   project_id  = var.project_id
   environment = var.environment
   secondary_ranges = {
-    test-main-subnet = [{
-      range_name    = "test-gke-pods-subnet",
-      ip_cidr_range = "10.10.2.0/23"
+    prod-main-subnet = [{
+      range_name    = "${var.environment}-gke-pods-subnet",
+      ip_cidr_range = "10.11.0.0/16"
       },
       {
-        range_name    = "test-gke-svcs-subnet",
-        ip_cidr_range = "10.10.4.0/24"
+        range_name    = "${var.environment}-gke-svcs-subnet",
+        ip_cidr_range = "10.12.0.0/20"
     }]
   }
   subnets = [{
-    subnet_name           = "test-main-subnet",
-    subnet_ip             = "10.10.0.0/23",
+    subnet_name           = "${var.environment}-main-subnet",
+    subnet_ip             = "10.10.0.0/16",
     subnet_region         = "europe-west2",
     subnet_private_access = "true",
     description           = "Main subnet"
   }]
   databases = {
     "intervene-${var.environment}" = {
-      deletion_protection_enabled = false
-      private_address             = "10.10.0.25",
+      private_address = "10.10.0.25",
       additional_users = [{
         name            = "intervene-${var.environment}",
         password        = null,
         random_password = true
       }]
+      backup_configuration = {
+        enabled = false
+      }
+      instance_type               = "db-f1-micro"
+      deletion_protection_enabled = false
     }
   }
-  use_sql_proxy = true
+  use_sql_proxy               = true
+  cluster_deletion_protection = false
 }
